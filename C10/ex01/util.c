4aa0062c78ba29da1efeca60dda882e3bb89c0c3
Ismail Ait bella <iait-bel@e1r11p2.1337.ma>
2021-10-12 12:35:31 +0100

    fix my cat haha

--- C10/ex01/util.c
+++ C10/ex01/util.c
@@ -23,16 +23,19 @@ int print_file(char *filename)
 	if (errno)
 	{
 		str = strerror(errno);
-		write(1, "ft_cat", 6);
+		write(1, "ft_cat: ", 8);
+		write(1, filename, ft_strlen(filename));
+		write(1, ": ", 2);
 		write(1, str, ft_strlen(str));
+		write(1, "\n", 1);
 		errno = 0;
-		return -1;
+		return 1;
 	}
 	if (fp < 0)
-		return 0;
+		return 1;
 
 	while(read(fp, &c, 1))
 		write(1, &c, 1);
 	close(fp);
-	return 1;
+	return 0;
 }
